37a8c20a2c2f59f009011dc844df9857de2da777
jinglin.tan <1556207795@qq.com>
2022-07-06 20:17:12 +0800

    feat: demo debugger

---
+++ demo/index.js
@@ -0,0 +1,32 @@
+import { Formula } from '../src'
+
+const formula = new Formula()
+
+window.onload = () => {
+  const inputCache = localStorage.getItem('input')
+  const dataCache = localStorage.getItem('data')
+  if (inputCache) {
+    document.getElementById('input').value = inputCache
+  }
+  if (dataCache) {
+    document.getElementById('data').value = dataCache
+  }
+}
+
+window.onTextChange = (type) => {
+  const val = document.getElementById(type).value
+  localStorage.setItem(type, val)
+}
+
+document.getElementById('exec').addEventListener('click', () => {
+  const input = document.getElementById('input').value
+  const data = document.getElementById('data').value
+  let formatData
+  try {
+    if (data) formatData = JSON.parse(data)
+  } catch (error) {
+    return alert('data value with:' + error)
+  }
+  const res = formula.exec(input, formatData)
+  document.getElementById('output').value = res
+})
