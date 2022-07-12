a5c2f2ae20c5b4373aafdf42843b10d36927c60b
jinglin.tan <1556207795@qq.com>
2022-07-12 17:39:45 +0800

    feat: full support of data and text function

--- src/function/date.ts
+++ src/function/date.ts
@@ -1,37 +1,114 @@
+import dayjs from 'dayjs';
+import isoWeeksInYear from 'dayjs/plugin/isoWeeksInYear'
+import isLeapYear from 'dayjs/plugin/isLeapYear'
+import weekOfYear from 'dayjs/plugin/weekOfYear'
+dayjs.extend(isoWeeksInYear)
+dayjs.extend(isLeapYear)
+dayjs.extend(weekOfYear)
+
+const createDayjs = (timestamp: number): dayjs.Dayjs => {
+  // unix time
+  if (String(timestamp).length === 10) {
+    return dayjs.unix(timestamp)
+  } else {
+    return dayjs(timestamp)
+  }
+}
 
 export const DateFunctions = {
-  'DATE(': function () {
-    let date
-    if (arguments.length === 6) {
+  'DATE(': function(...args: Array<string | number>) {
+    let date;
+    const dateArr = [...args]
+    if (dateArr.length === 6) {
+      // year month day hour minute second
       date = new Date(
-        parseInt(arguments[0], 10),
-        parseInt(arguments[1], 10) - 1,
-        parseInt(arguments[2], 10),
-        parseInt(arguments[3], 10),
-        parseInt(arguments[4], 10),
-        parseInt(arguments[5], 10)
-      )
-    } else if (arguments.length === 3) {
+        parseInt(String(dateArr[0]), 10),
+        parseInt(String(dateArr[1]), 10) - 1,
+        parseInt(String(dateArr[2]), 10),
+        parseInt(String(dateArr[3]), 10),
+        parseInt(String(dateArr[4]), 10),
+        parseInt(String(dateArr[5]), 10),
+      );
+    } else if ([...args].length === 3) {
+    // year month day
       date = new Date(
-        parseInt(arguments[0], 10),
-        parseInt(arguments[1], 10) - 1,
-        parseInt(arguments[2], 10)
-      )
+        parseInt(String(dateArr[0]), 10),
+        parseInt(String(dateArr[1]), 10) - 1,
+        parseInt(String(dateArr[2]), 10),
+      );
     } else {
-      if (arguments[0] === undefined) {
-        return undefined
+    // timestamp
+      if (dateArr[0] === undefined) {
+        return undefined;
       }
-      date = new Date(arguments[0] * 1000)
+      date = createDayjs(Number(dateArr[0])).toDate();
     }
-    return date
+    return date;
   },
-  'TIME(': function (h: number, m: number, s: number) {
-    return (3600 * h + 60 * m + s) / 86400
+  'DATEDELTA(': function(a: number, b: number): number {
+    return a + b * (60 * 60 * 24);
   },
-  'TIMESTAMP(': function (a: Date | number) {
-    return Number(a) / 1000
+  'DAY(': function(time: number): number | undefined {
+    if (time === undefined) return;
+    return createDayjs(time).day();
   },
-  'NOW(': function () {
-    return parseInt(String(new Date().getTime() / 1000))
+  'DAYS(': function(start: number, end: number): number | undefined {
+    if (start === undefined || end === undefined) return;
+    const startTime = createDayjs(start);
+    const endTime = createDayjs(end).format('YYYY-MM-DD');
+    return startTime.diff(endTime, 'day');
   },
+  'HOUR(': function(timestamp: number) {
+    if (timestamp === undefined) return
+    return createDayjs(timestamp).hour()
+  },
+  'ISOWEEKNUM(': function(timestamp: number) {
+    return createDayjs(timestamp).isoWeeksInYear()
+  },
+  'MINUTE(': function(timestamp: number) {
+    if (timestamp === undefined) return
+    return createDayjs(timestamp).minute()
+  },
+  'MONTH(': function(timestamp: number) {
+    if (timestamp === undefined) return
+    return createDayjs(timestamp).month()
+  },
+  'TIME(': function(h: number, m: number, s: number) {
+    return (3600 * h + 60 * m + s) / 86400;
+  },
+  'TIMESTAMP(': function(a: Date | number) {
+    return Number(a) / 1000;
+  },
+  'NOW(': function() {
+    return parseInt(String(new Date().getTime()));
+  },
+  'ONEYEARRANGDAY(': function(start: number, end: number, year: string) {
+    if (!start || !end) return 0
+    const startTime = createDayjs(start);
+    const endTime = createDayjs(end);
+    if (!year) return startTime.diff(endTime.format('YYYY-MM-DD'), 'day');
+    let minTime = +start - +end > 0 ? +end : +end
+    let maxTime = +start - +end > 0 ? +end : +end
+    // the year's first day
+    let preDate = String(start).length === 10 ? dayjs(`${year}-1-1 00:00:00`).unix() : dayjs(`${year}-1-1 00:00:00`)
+    // the year's last day
+    let nextDate = String(start).length === 10 ? dayjs(`${year}-12-12 23:59:59`).unix() : dayjs(`${year}-12-12 23:59:59`)
+    if (minTime > nextDate || maxTime < preDate) {
+      return 0
+    }
+    let arr = [minTime, maxTime, preDate, nextDate].sort()
+    return (Number(arr[2]) - Number(arr[1])) / (1 * 60 * 60 * 24)
+  },
+  'TODAY(': function() {
+    return new Date().getTime()
+  },
+  'WEEKDAY': function(timestamp: number) {
+    return createDayjs(timestamp).date();
+  },
+  'WEEKNUM': function(timestamp: number) {
+    return createDayjs(timestamp).week();
+  },
+  'YEAR': function(timestamp: number) {
+    return createDayjs(timestamp).year();
+  }
 };
--- src/function/text.ts
+++ src/function/text.ts
@@ -1,3 +1,81 @@
+const isObjectEmpty = function(a: any) {
+  if (a == null) {
+    return !0;
+  }
+  if (a.length > 0) {
+    return !1;
+  }
+  if (a.length === 0) {
+    return !0;
+  }
+  for (var b in a) {
+    if (Object.hasOwnProperty.call(a, b)) {
+      return !1;
+    }
+  }
+  return isNaN(a);
+};
+
 export const TextFunctions = {
-  'JOIN(': (params: Array<any>, split: string) => params.join(split)
+  'JOIN(': (params: Array<any>, split: string) => params.join(split),
+  'CONCATENATE(': function(...args: Array<string>) {
+    let res = '';
+    [...args].forEach((item = '') => {
+      res += item;
+    });
+    return res;
+  },
+  'EXACT(': function(text_1: string, text_2: string) {
+    return text_1 === text_2;
+  },
+  'ISEMPTY(': function(target: any) {
+    return isObjectEmpty(target);
+  },
+  'LEFT(': function(text = '', slice: number) {
+    return ''.substr.call(text, 0, slice);
+  },
+  'LEN(': function(text: string) {
+    return text.length;
+  },
+  'LOWER(': function (text: string = '') {
+    return text.toLocaleLowerCase()
+  },
+  'MID(': function(text: string, start: number, end: number) {
+    return text.slice(start - 1, start + end - 1)
+  },
+  'REPLACE(': function(text = '', start: number, end: number, target: string) {
+    let arr = text.split('')
+    arr.splice(start - 1, end, target)
+    return arr.join('')
+  },
+  'REPT': function(text: string, times: number) {
+    let str = ''
+    for (let i = 0; i < times; i++) {
+      str += text
+    }
+    return str
+  },
+  'RIGHT(': function(text = '', lens: number) {
+    return text.substr(-lens)
+  },
+  'SEARCH(': function(text1 = '', text2 = '') {
+    return text2.indexOf(text1) + 1
+  },
+  'SPLIT(': function (text = '', split = '') {
+    return text.split(split)
+  },
+  // TODO: check full function
+  'TEXT(': function(number: number) {
+    return String(number)
+  },
+  'TRIM(': function(text = '') {
+    return text.trim().replace(/\s+/g, '')
+  },
+  'UPPER(': function(text = '') {
+    return text.toLocaleUpperCase()
+  },
+  // TODO: Type check
+  'VALUE(': function(text = '') {
+    return Number(text)
+  }
 };
