1492fd5cdbe8e9dd2770eb5f9bbfa4d12719cdb1
耿子怡 <ziyi.geng@xbongbong.com>
2023-11-10 15:33:47 +0800

    fix: 修复TEXT数字转大写金额不正确

--- src/utils/convertCurrency.ts
+++ src/utils/convertCurrency.ts
@@ -1,35 +1,36 @@
-export const convertCurrency = (num: number) => {
-  var fraction = ["角", "分", "厘", "毫"];
-  var digit = ["零", "壹", "贰", "叁", "肆", "伍", "陆", "柒", "捌", "玖"];
-  var unit = [
-    ["元", "万", "亿"],
-    ["", "拾", "佰", "仟"],
-  ];
-  var head = num < 0 ? "负" : "";
-  num = Math.abs(num);
-  var result = "";
-  for (var i = 0; i < fraction.length; i++) {
-    console.log("pow", Math.floor(num * 10 * Math.pow(10, i)) % 10);
-    result += (
-      digit[Math.floor(num * 10 * Math.pow(10, i)) % 10] + fraction[i]
-    ).replace(/零./, "");
-  }
-  result = result || "整";
-  num = Math.floor(num);
-  for (var i = 0; i < unit[0].length && num > 0; i++) {
-    var p = "";
-    for (var j = 0; j < unit[1].length && num > 0; j++) {
-      p = digit[num % 10] + unit[1][j] + p;
-      num = Math.floor(num / 10);
-    }
-    result =
-      p.replace(/(零.)*零$/, "").replace(/^$/, "零") + unit[0][i] + result;
-  }
-  return (
-    head +
-    result
-      .replace(/(零.)*零元/, "元")
-      .replace(/(零.)+/g, "零")
-      .replace(/^整$/, "零元整")
-  );
+export const convertCurrency = (n) => {
+   var fraction = ['角', '分', '厘', '毫'];
+   var digit = [
+      '零', '壹', '贰', '叁', '肆',
+      '伍', '陆', '柒', '捌', '玖'
+   ];
+   var unit = [
+      ['元', '万', '亿'],
+      ['', '拾', '佰', '仟']
+   ];
+   var head = n < 0 ? '负' : '';
+   n = Math.abs(n);
+   var s = n.toString();
+   console.log('s', s.split('.'))
+   var integerPart = s.split('.')[0];
+   var decimalPart = s.split('.')[1] || '';
+   var r = r || "整";
+   for (var i = 0; i < unit[0].length && integerPart > 0; i++) {
+      var t = '';
+      for (var j = 0; j < unit[1].length && integerPart > 0; j++) {
+         t = digit[integerPart % 10] + unit[1][j] + t;
+         integerPart = Math.floor(integerPart / 10);
+      }
+      r = t.replace(/(零.)*零$/, '').replace(/^$/, '零') + unit[0][i] + r;
+   }
+   console.log('r', r)
+   return (
+      head +
+      r.replace(/(零.)*零元/, '元')
+         .replace(/(零.)+/g, '零')
+         .replace(/^整$/, '零元整') +
+      decimalPart.split('').map(function (v, i) {
+         return (digit[v] + fraction[i]);
+      }).join('')
+   );
 };
