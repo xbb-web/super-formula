47a7de08351953a6067a2931c2cf682927b020f0
耿子怡 <ziyi.geng@xbongbong.com>
2023-11-10 13:54:19 +0800

    fix: 修复函数参数为负数，语法拆分错误

--- src/lexer.ts
+++ src/lexer.ts
@@ -7,26 +7,26 @@ import { createToken, Lexer, TokenType } from 'chevrotain';
 export const AddSubSeatMark = createToken({ name: 'AddSubSeatMark', pattern: Lexer.NA });
 export const MutDivSeatMark = createToken({ name: 'MutDivSeatMark', pattern: Lexer.NA });
 export const CompareSeatMark = createToken({ name: 'CompareSeatMark', pattern: Lexer.NA });
-export const VariableSeatMark = createToken({ name:  'VariableSeatMark', pattern: Lexer.NA })
+export const VariableSeatMark = createToken({ name: 'VariableSeatMark', pattern: Lexer.NA })
 
 /**
  * RideMark
  * @desc eg: a * b
  */
 export const MutMark = createToken({
-  name: 'MutMark',
-  pattern: /\*/,
-  categories: MutDivSeatMark,
+   name: 'MutMark',
+   pattern: /\*/,
+   categories: MutDivSeatMark,
 });
 
 /**
  * DivMark
  * @desc eg: a / b
  */
- export const DivMark = createToken({
-  name: 'DidMark',
-  pattern: /\//,
-  categories: MutDivSeatMark,
+export const DivMark = createToken({
+   name: 'DidMark',
+   pattern: /\//,
+   categories: MutDivSeatMark,
 });
 
 /**
@@ -34,9 +34,9 @@ export const MutMark = createToken({
  * @desc eg: a + b
  */
 export const AddMark = createToken({
-  name: 'AddMark',
-  pattern: /\+/,
-  categories: AddSubSeatMark,
+   name: 'AddMark',
+   pattern: /\+/,
+   categories: AddSubSeatMark,
 });
 
 /**
@@ -44,9 +44,9 @@ export const AddMark = createToken({
  * @desc eg: a - b
  */
 export const SubMark = createToken({
-  name: 'SubMark',
-  pattern: /\-/,
-  categories: AddSubSeatMark,
+   name: 'SubMark',
+   pattern: /(\d+)-(\d+)/,
+   categories: AddSubSeatMark,
 });
 
 /**
@@ -54,9 +54,9 @@ export const SubMark = createToken({
  * @desc eg: a == b
  */
 export const EqualMark = createToken({
-  name: 'EqualMark',
-  pattern: /\=\=/,
-  categories: CompareSeatMark,
+   name: 'EqualMark',
+   pattern: /\=\=/,
+   categories: CompareSeatMark,
 });
 
 
@@ -64,10 +64,10 @@ export const EqualMark = createToken({
  * EqualMark
  * @desc eg: a != b
  */
- export const UnEqualMark = createToken({
-  name: 'UnEqualMark',
-  pattern: /!\=/,
-  categories: CompareSeatMark,
+export const UnEqualMark = createToken({
+   name: 'UnEqualMark',
+   pattern: /!\=/,
+   categories: CompareSeatMark,
 });
 
 /**
@@ -75,9 +75,9 @@ export const EqualMark = createToken({
  * @desc eg: a > b. Important: it registration order must take precedence over GteMark.
  */
 export const GtMark = createToken({
-  name: 'GtMark',
-  pattern: />/,
-  categories: CompareSeatMark,
+   name: 'GtMark',
+   pattern: />/,
+   categories: CompareSeatMark,
 });
 
 /**
@@ -85,9 +85,9 @@ export const GtMark = createToken({
  * @desc eg: a < b. Important: it registration order must take precedence over LteMark.
  */
 export const LtMark = createToken({
-  name: 'LtMark',
-  pattern: /</,
-  categories: CompareSeatMark,
+   name: 'LtMark',
+   pattern: /</,
+   categories: CompareSeatMark,
 });
 
 /**
@@ -95,9 +95,9 @@ export const LtMark = createToken({
  * @desc eg: a >= b.
  */
 export const GteMark = createToken({
-  name: 'GteMark',
-  pattern: />=/,
-  categories: CompareSeatMark,
+   name: 'GteMark',
+   pattern: />=/,
+   categories: CompareSeatMark,
 });
 
 /**
@@ -105,9 +105,9 @@ export const GteMark = createToken({
  * @desc eg: a >= b.
  */
 export const LteMark = createToken({
-  name: 'LteMark',
-  pattern: /<=/,
-  categories: CompareSeatMark,
+   name: 'LteMark',
+   pattern: /<=/,
+   categories: CompareSeatMark,
 });
 
 /**
@@ -115,8 +115,8 @@ export const LteMark = createToken({
  * @desc split for function sentence
  */
 export const CommaMark = createToken({
-  name: 'CommaMark',
-  pattern: /,/,
+   name: 'CommaMark',
+   pattern: /,/,
 });
 
 /**
@@ -124,9 +124,9 @@ export const CommaMark = createToken({
  * @desc Variable analysis, Extract values from objects
  */
 export const VariableMark = createToken({
-  name: 'VariableMark',
-  pattern: /{.*?}/,
-  categories: VariableSeatMark
+   name: 'VariableMark',
+   pattern: /{.*?}/,
+   categories: VariableSeatMark
 });
 
 /**
@@ -134,15 +134,15 @@ export const VariableMark = createToken({
  * @desc pure number for CONSUME
  */
 export const NumberMark = createToken({
-  name: 'NumberMark',
-  pattern: /-?\d*\.?\d+/,
-  categories: VariableSeatMark
+   name: 'NumberMark',
+   pattern: /-?\d*\.?\d+/,
+   categories: VariableSeatMark
 });
 
 export const StringMark = createToken({
-  name: 'StringMark',
-  pattern: /["|'][\u4e00-\u9fa5a-zA-Z0-9\uFF00-\uFFEF\u3000-\u303F\uFF01-\uFF5Ea-zA-Z0-9=><!/,!@#$%^&*,.(){}\\|;:<>/?~`\[\]-]+["|']/,
-  categories: VariableSeatMark
+   name: 'StringMark',
+   pattern: /["|'][\u4e00-\u9fa5a-zA-Z0-9\uFF00-\uFFEF\u3000-\u303F\uFF01-\uFF5Ea-zA-Z0-9=><!/,!@#$%^&*,.(){}\\|;:<>/?~`\[\]-]+["|']/,
+   categories: VariableSeatMark
 })
 
 /**
@@ -150,8 +150,8 @@ export const StringMark = createToken({
  * @desc work for Function CONSUME, PS: it need register before StartParen, because they have a parcel relationship
  */
 export const FunctionMark = createToken({
-  name: 'Function',
-  pattern: /[A-Za-z_]+[A-Za-z_0-9.]*\(/,
+   name: 'Function',
+   pattern: /[A-Za-z_]+[A-Za-z_0-9.]*\(/,
 });
 
 /**
@@ -159,8 +159,8 @@ export const FunctionMark = createToken({
  * @desc Work for number calculate, used to improve the priority of calculation
  */
 export const StartParen = createToken({
-  name: 'StartParen',
-  pattern: /\(/
+   name: 'StartParen',
+   pattern: /\(/
 });
 
 /**
@@ -168,8 +168,8 @@ export const StartParen = createToken({
  * @desc Work with FunctionMark and StartParen
  */
 export const CloseParen = createToken({
-  name: 'CloseParen',
-  pattern: /\)/
+   name: 'CloseParen',
+   pattern: /\)/
 });
 
 /**
@@ -186,8 +186,8 @@ export const CloseParen = createToken({
  * @desc Start tag of the array
  */
 export const ArrayStartMark = createToken({
-  name: 'ArrayStartMark',
-  pattern: /\[/,
+   name: 'ArrayStartMark',
+   pattern: /\[/,
 })
 
 /**
@@ -195,8 +195,8 @@ export const ArrayStartMark = createToken({
  * @desc End tag of the array
  */
 export const ArrayEndMark = createToken({
-  name: 'ArrayEndMark',
-  pattern: /\]/,
+   name: 'ArrayEndMark',
+   pattern: /\]/,
 })
 
 /** 
@@ -204,44 +204,44 @@ export const ArrayEndMark = createToken({
  * @desc We will skipped all white space for input
  */
 const WhiteSpace = createToken({
-  name: 'WhiteSpace',
-  pattern: /\s+/,
-  group: Lexer.SKIPPED,
+   name: 'WhiteSpace',
+   pattern: /\s+/,
+   group: Lexer.SKIPPED,
 });
 
 export const AllLexerToken = [
-  AddSubSeatMark,
-  MutDivSeatMark,
-  CompareSeatMark,
-  MutMark,
-  SubMark,
-  DivMark,
-  AddMark,
-  SubMark,
-  GteMark,
-  LteMark,
-  GtMark,
-  LtMark,
-  EqualMark,
-  UnEqualMark,
-  WhiteSpace,
-  CommaMark,
-  FunctionMark,
-  StartParen,
-  CloseParen,
-  VariableSeatMark,
-  NumberMark,
-  VariableMark,
-  StringMark,
-  // ArrayMark,
-  ArrayStartMark,
-  ArrayEndMark
+   AddSubSeatMark,
+   MutDivSeatMark,
+   CompareSeatMark,
+   MutMark,
+   SubMark,
+   DivMark,
+   AddMark,
+   SubMark,
+   GteMark,
+   LteMark,
+   GtMark,
+   LtMark,
+   EqualMark,
+   UnEqualMark,
+   WhiteSpace,
+   CommaMark,
+   FunctionMark,
+   StartParen,
+   CloseParen,
+   VariableSeatMark,
+   NumberMark,
+   VariableMark,
+   StringMark,
+   // ArrayMark,
+   ArrayStartMark,
+   ArrayEndMark
 ]
 
 export const TokenVocabulary: Record<string, TokenType> = {}
 for (let i = 0; i < AllLexerToken.length; i++) {
-  const element = AllLexerToken[i];
-  TokenVocabulary[element.name] = element
+   const element = AllLexerToken[i];
+   TokenVocabulary[element.name] = element
 }
 
 export const FormulaLexer = new Lexer(AllLexerToken)
